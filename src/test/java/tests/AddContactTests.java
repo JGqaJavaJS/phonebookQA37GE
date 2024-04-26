e7a8bdcfdceff4640c6abd74ffd49ad71542fedf
julia <juliiagordin@gmail.com>
2024-04-26 13:42:43 +0300

    add logger and screenshot and data provider

--- src/test/java/tests/AddContactTests.java
+++ src/test/java/tests/AddContactTests.java
@@ -4,7 +4,11 @@ import dto.ContactDTO;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+import utils.DataProviders;
+
+import java.util.Random;
 
 public class AddContactTests extends BaseTest{
 
@@ -22,11 +26,16 @@ public class AddContactTests extends BaseTest{
 
     @Test
     public void positiveAddContact() {
+        Random random = new Random();
+        String name = "John" + String.valueOf(random.nextInt(1000));
+        String phone = String.valueOf(random.nextInt(10000)) +"56789";
+        //String phone2 = String.valueOf(random.nextLong(1000000000L));
+
         app.getContactHelper().clickAddOnNavBar();
         ContactDTO contactDTO = new ContactDTO()
-                .setName("dsafadfsdcDASD")
+                .setName(name)
                 .setLastName("jhsbfh")
-                .setPhone("5554567890")
+                .setPhone(phone)
                 .setEmail("sjkfhj@mail.com")
                 .setAddress("hjsbfhb")
                 .setDescription("bhjfdb");
@@ -34,7 +43,36 @@ public class AddContactTests extends BaseTest{
 
         app.getContactHelper().pause(3000);
 
-        Assert.assertTrue(app.getContactHelper().isContactDisplaysOnThePage("5554567890"));
+        Assert.assertTrue(app.getContactHelper().isContactDisplaysOnThePage(phone));
+    }
+
+    @Test(dataProvider = "addNewContact", dataProviderClass = DataProviders.class)
+    public void positiveAddContactProvider(String name, String lastName, String phone,
+                                           String email, String address, String description)
+    {
+        app.getContactHelper().clickAddOnNavBar();
+        ContactDTO contactDTO = new ContactDTO()
+                .setName(name)
+                .setLastName(lastName)
+                .setPhone(phone)
+                .setEmail(email)
+                .setAddress(address)
+                .setDescription(description);
+        app.getContactHelper().addContact(contactDTO);
+
+        app.getContactHelper().pause(2000);
+
+        Assert.assertTrue(app.getContactHelper().isContactDisplaysOnThePage(phone));
+    }
+
+    @Test(dataProvider = "addContactCSVFile", dataProviderClass = DataProviders.class)
+    public void positiveAddContactProviderCSV(ContactDTO contactDTO)
+    {
+        app.getContactHelper().clickAddOnNavBar();
+        app.getContactHelper().addContact(contactDTO);
+        app.getContactHelper().pause(2000);
+        Assert.assertTrue(app.getContactHelper()
+                .isContactDisplaysOnThePage(contactDTO.getPhone()));
     }
 
 }
