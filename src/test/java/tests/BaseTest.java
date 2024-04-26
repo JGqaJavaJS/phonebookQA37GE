e31ec16744cdc5f243eb739b000bb8d757e10df9
julia <juliiagordin@gmail.com>
2024-04-26 11:46:10 +0300

    add logger implementation

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -3,6 +3,8 @@ package tests;
 import config.ApplicationManager;
 import dto.UserDTO;
 import org.openqa.selenium.remote.Browser;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 
@@ -10,13 +12,18 @@ public class BaseTest {
     static ApplicationManager app =
             new ApplicationManager(System
                     .getProperty("browser", Browser.CHROME.browserName()));
+
+    static Logger logger = LoggerFactory.getLogger(BaseTest.class);
+
     UserDTO user = new UserDTO().setEmail("testqa20@gmail.com").setPassword("123456Aa$");
     @BeforeSuite
     public static void startBrowser() {
+        logger.info("run browser settings ");
         app.init();
     }
     @AfterSuite
     public static void tearDown() {
+        logger.info("quit browser");
         app.quit();
     }
 
